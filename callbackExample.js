42104bdd665cf6e6d5ba9eb6f414bc2916437707
Unknown <luke.towell@hotmail.com>
2018-12-05 23:28:59 +0000

    Added in a little bit more to the callback
    - Added in a better example of a callback.

--- callbackExample.js
+++ callbackExample.js
@@ -1,15 +1,38 @@
-console.log('before');
+console.log('Start');
+
+//retrieve user information
 getUser(1, (user) => {
     console.log('user', user);
 });
-console.log("after");
+
+// retrieve fictional user address
+getAddress("luke", (address) => {
+    console.log("Luke", address);
+})
+
+// end of files
+console.log("End");
 
 function getUser(id, callback){
     setTimeout(() => {
         console.log("called");
         callback({
             id: id,
-            username: "luke"
+            firstName: "Bilbo",
+            lastName: "Baggins"
+        });
+    }, 2000)
+}
+
+function getAddress(username, callback){
+    setTimeout(()=> {
+        console.log("Retrieving Address");
+        callback({
+            firstLineAddress: "Bag End",
+            secondLineAddress: "Under The Hill",
+            State: "Hobbiton",
+            Region: "The Shire",
+            Country: "Middle Earth"
         });
     }, 2000)
 }
